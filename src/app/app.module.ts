19716cd47cbce1ab94e1c4210e7a270bcc3a8cea
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-11-29 22:13:23 +0300

    add component signIn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { FormComponent } from "./form/form.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
 
 import { Routes, RouterModule } from "@angular/router";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "form", component: FormComponent }
+  { path: "form", component: FormComponent },
+  { path: "signIn", component: SignInComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FormComponent],
+  declarations: [AppComponent, HomeComponent, FormComponent, SignInComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
